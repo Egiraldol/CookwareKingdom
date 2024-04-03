63be23269666ec5364810fa3e079f46908bd1195
Esteban <estebangiraldollano@gmail.com>
2024-04-02 23:03:16 -0500

    fix: product/show/ layout

--- resources/views/product/show.blade.php
+++ resources/views/product/show.blade.php
@@ -4,7 +4,7 @@
 @section('title', $viewData["title"])
 @section('content')
 <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
-<div class="row">
+<div class="row mx-3">
   <div class="col-md-4">
     <div class="card mb-3">
       <img src="{{ $viewData["product"]->getImages() }}" class="card-img-top" alt="Product image">
@@ -52,41 +52,45 @@
   </div>
 </div>
 
-<div class="card mb-3">
-  <div class="card-body">
-    <div class="accordion" id="accordionExample">
-      <div class="accordion-item">
-          <h2 class="accordion-header">
-            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-              Reviews
-            </button>
-          </h2>
-          <div id="collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
-            <div class="accordion-body">
-              <ul>
-              @if ($viewData['product']->getReviews()->isEmpty())
-                  <p>This product has no reviews</p>
-              @else
+<div class="row mx-3">
+  <div class="col-md-12">
+    <div class="card mb-3 mt-3">
+      <div class="card-body">
+        <div class="accordion" id="accordionExample">
+          <div class="accordion-item">
+              <h2 class="accordion-header">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                  Reviews
+                </button>
+              </h2>
+              <div id="collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                <div class="accordion-body">
                   <ul>
-                      @foreach ($viewData['product']->getReviews() as $review)
-                      <ol class="list-group list-group-numbered">
-                        <li class="list-group-item d-flex justify-content-between align-items-start">
-                          <div class="ms-2 me-auto">
-                            <div class="fw-bold">{{ $review->getTitle() }}</div>
-                            <div class="small">By {{ $review->getName() }}:</div>
-                            <div>{{ $review->getDescription() }}</div>
-                          </div>
-                          <span class="badge custom-badge rounded-pill">{{ $review->getRating() }}</span>
-                        </li>
-                      </ol>
-                      @endforeach
+                  @if ($viewData['product']->getReviews()->isEmpty())
+                      <p>This product has no reviews</p>
+                  @else
+                      <ul>
+                          @foreach ($viewData['product']->getReviews() as $review)
+                          <ol class="list-group list-group-numbered">
+                            <li class="list-group-item d-flex justify-content-between align-items-start">
+                              <div class="ms-2 me-auto">
+                                <div class="fw-bold">{{ $review->getTitle() }}</div>
+                                <div class="small">By {{ $review->getName() }}:</div>
+                                <div>{{ $review->getDescription() }}</div>
+                              </div>
+                              <span class="badge custom-badge rounded-pill">{{ $review->getRating() }}</span>
+                            </li>
+                          </ol>
+                          @endforeach
+                      </ul>
+                  @endif
                   </ul>
-              @endif
-              </ul>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
       </div>
+    </div>
   </div>
 </div>
 @endsection
