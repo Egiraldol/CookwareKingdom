d2a8c59ef839075aaa510c12c7e01b77e868f712
Egiraldol <Estebangiraldollano@gmail.com>
2024-05-12 20:36:00 -0500

    feat: added breadcrumbs to user page

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -4,6 +4,11 @@
 @section("title", $viewData["title"])
 @section("subtitle", $viewData["subtitle"])
 @section('content')
+
+<div class="mx-3">
+    {{ Breadcrumbs::render('cart.index') }}
+</div>
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
